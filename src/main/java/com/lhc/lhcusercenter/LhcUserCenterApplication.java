d950f623b200e55eef8d52df6b392921164dd05a
liuhc652 <Liu5990733@qq.com>
2024-08-16 16:16:46 +0800

    init

---
+++ src/main/java/com/lhc/lhcusercenter/LhcUserCenterApplication.java
@@ -0,0 +1,13 @@
+package com.lhc.lhcusercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhcUserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LhcUserCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhc/lhcusercenter/LhcUserCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhc.lhcusercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhcUserCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
